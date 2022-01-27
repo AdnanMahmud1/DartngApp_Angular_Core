8eb1be1e92e8be98eadef82b277d3a3b6d2a6270
11adn <11adn@DESKTOP-1L9UHQT>
2022-01-27 18:19:01 +0600

    Add project files.

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,31 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public UsersController(DataContext Context)
+        {
+            _context = Context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<AppUser>> GetUsers()
+        {
+            return _context.Users.ToList();
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<AppUser> GetUser(int id)
+        {
+            return _context.Users.Find(id);
+        }
+    }
+}
