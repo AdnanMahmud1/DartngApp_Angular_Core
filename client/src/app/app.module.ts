80c9f561cb7894e0aa2d946a9e58788d7ae03ab1
Adnan Mahmud <11adnan@live.com>
2022-02-23 17:23:58 +0600

    Centralize error handles

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -16,6 +16,7 @@ import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
 import { ToastrModule } from 'ngx-toastr';
 import { SharedModule } from './_modules/shared.module';
+import { TestErrorsComponent } from './errors/test-errors/test-errors.component';
 
 
 
@@ -30,6 +31,7 @@ import { SharedModule } from './_modules/shared.module';
     MemeberDetailComponent,
     ListsComponent,
     MessagesComponent,
+    TestErrorsComponent,
    
   ],
   imports: [
