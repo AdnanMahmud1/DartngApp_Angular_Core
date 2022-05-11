39c0aa6084025ee4b45f34d439b4686c639d56c0
Adnan Mahmud <11adnan@live.com>
2022-05-11 16:52:23 +0600

    End of serction 13

--- client/src/app/members/memeber-list/memeber-list.component.ts
+++ client/src/app/members/memeber-list/memeber-list.component.ts
@@ -23,11 +23,8 @@ export class MemeberListComponent implements OnInit {
   user: User;
   genderList = [{value:'male', display: 'Males'}, {value:'female', display: 'Females'}];
 
-  constructor(private memberService: MembersService, private accountService: AccountService) {
-    this.accountService.currentUser$.pipe(take(1)).subscribe(user => {
-      this.user = user;
-      this.userParams = new UserParams(user);
-    })
+  constructor(private memberService: MembersService) {
+   this.userParams = memberService.getUserParams();
    }
 
   ngOnInit(): void {
@@ -40,17 +37,20 @@ export class MemeberListComponent implements OnInit {
   //   })
   // }
  loadMembers(){
+  this.memberService.setUserParams(this.userParams);
    this.memberService.getMembers(this.userParams).subscribe(response =>{
      this.members = response.result;
      this.pagination =response.pagination;
    })
  }
  resetFilters(){
-   this.userParams =new UserParams(this.user);
+   this.userParams = this.memberService.resetUserParams();
    this.loadMembers();
  }
+ 
  pageChanged(event: any){
    this.userParams.pageNumber = event.page;
+   this.memberService.setUserParams(this.userParams);
    this.loadMembers();
  }
 
