cf22880b920b9586443f9b40dea84636b0ddd56e
Adnan <Adnan@DESKTOP-UGPNVH0>
2022-10-02 16:04:04 +0600

    Query optimization started

--- API/Helpers/AutoMapperProfiles.cs
+++ API/Helpers/AutoMapperProfiles.cs
@@ -24,8 +24,8 @@ namespace API.Helpers
                     opt => opt.MapFrom(src => 
                         src.Recipient.Photos.FirstOrDefault(x => x.IsMain).Url));
 
-            CreateMap<DateTime, DateTime>()
-                .ConvertUsing(d=>DateTime.SpecifyKind(d, DateTimeKind.Utc));
+            //CreateMap<DateTime, DateTime>()
+            //    .ConvertUsing(d=>DateTime.SpecifyKind(d, DateTimeKind.Utc));
         }
     }
 }
